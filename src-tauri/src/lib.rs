bde51c18585584924c68aa105add87e5c2885606
MaciejkaG <me@mcjk.cc>
2025-06-18 23:55:18 +0200

    Add temporary logos, start implementing styling

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::{WebviewUrl, WebviewWindowBuilder};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,6 +9,41 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+    .setup(|app| {
+      let win_builder =
+        WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+          .title("Transparent Titlebar Window")
+          .inner_size(800.0, 600.0)
+          .decorations(false);
+
+      // set transparent title bar only when building for macOS
+      #[cfg(target_os = "macos")]
+      let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
+
+      let window = win_builder.build().unwrap();
+
+      // set background color only when building for macOS
+      #[cfg(target_os = "macos")]
+      {
+        use tauri::{TitleBarStyle};
+        use cocoa::appkit::{NSColor, NSWindow};
+        use cocoa::base::{id, nil};
+
+        let ns_window = window.ns_window().unwrap() as id;
+        unsafe {
+          let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+              nil,
+              50.0 / 255.0,
+              158.0 / 255.0,
+              163.5 / 255.0,
+              1.0,
+          );
+          ns_window.setBackgroundColor_(bg_color);
+        }
+      }
+
+      Ok(())
+    })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "./fonts.css";
+import DragBar from "./components/DragBar";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <DragBar />
     <App />
   </React.StrictMode>,
 );
