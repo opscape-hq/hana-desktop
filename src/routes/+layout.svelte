ff823e72de87c2c67d3ef41428b79ab0101f3462
Maciej Gomoła <me@mcjk.cc>
2025-09-12 14:48:36 +0200

    start changing design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,18 +7,18 @@
 
 <div class="flex flex-col h-screen">
   <!-- Titlebar -->
-  <div class="select-none h-10 border-b box-border text-sm flex">
+  <div class="select-none h-10 box-border text-sm flex bg-titlebar-background">
     <!-- Traffic lights space -->
-    <div class="w-[74px] border-r"></div>
+    <div class="w-[74px]"></div>
 
     <div class="flex flex-1" data-tauri-drag-region>
-      <div class="px-2 flex items-center border-r">
+      <div class="px-2 flex items-center rounded-t-lg bg-background">
         <span>root@192.168.0.1 — neofetch</span>
       </div>
     </div>
   </div>
 
-  <div class="flex-1 bg-red-500" transition:slide>
+  <div class="flex-1 bg-background" transition:slide>
     {@render children()}
   </div>
 </div>
